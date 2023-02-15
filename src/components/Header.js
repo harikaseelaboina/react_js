73b3949e4874ca4f24f441f440c5545e2235bb29
harikaseelaboina <harika.krishna99@gmail.com>
2023-02-16 00:23:44 +0530

    new changes added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
 
-const Header=()=>{
+const Header = () => {
     return (
         <div className="headcontainer">
             <h1 className="mainhead1">Contact Manager</h1>
+            <p>please enter your details</p>
         </div>
     )
 }
